dc7dc34833ea4f00f01c767fe9f89d94a8d4d846
Gabriel Rivera <gabriel@worksimpli.io>
2026-04-01 17:18:18 -0400

    feat: update nvim config with lsp keybindings for vscode neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,17 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
   vim.opt.clipboard = "unnamedplus"
 
   vim.g.mapleader = " "
   vim.g.maplocalleader = "\\"
+
+  local vscode = require("vscode")
+
+  -- LSP Navigation (these call VS Code's native LSP instead of Neovim's)
+  vim.keymap.set("n", "gd", function() vscode.action("editor.action.revealDefinition") end)
+  vim.keymap.set("n", "gr", function() vscode.action("editor.action.goToReferences") end)
+  vim.keymap.set("n", "gi", function() vscode.action("editor.action.goToImplementation") end)
+  vim.keymap.set("n", "gt", function() vscode.action("editor.action.goToTypeDefinition") end)
+  vim.keymap.set("n", "gD", function() vscode.action("editor.action.peekDefinition") end)
 else
   require("config.lazy")
 end
